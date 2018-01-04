9936ce6a870c44d06710105b9ef01b159a451b4e
Neri <leonardo.v.neri@accenture.com>
2018-01-04 17:44:57 -0300

    Refactor code to PEP8 standards.

--- createDataset.py
+++ createDataset.py
@@ -4,155 +4,161 @@ import os
 import re
 from datetime import datetime
 
-personName = input('Enter your full name: ')
-fbData = input('Do you have Facebook data to parse through (y/n)?')
-googleData = input('Do you have Google Hangouts data to parse through (y/n)?')
-linkedInData = input('Do you have LinkedIn data to parse through (y/n)?')
+person_name = input('Enter your full name: ')
+fb_data = input('Do you have Facebook data to parse through (y/n)?')
+google_data = input('Do you have Google Hangouts data to parse through (y/n)?')
+linkedin_data = input('Do you have LinkedIn data to parse through (y/n)?')
 
-def getGoogleHangoutsData():
+
+def get_google_hangouts_data():
     # Putting all the file names in a list
-    allFiles = []
+    all_files = []
     # Edit these file and directory names if you have them saved somewhere else
     for filename in os.listdir('GoogleTextForm'):
         if filename.endswith(".txt"):
-            allFiles.append('GoogleTextForm/' + filename)
+            all_files.append('GoogleTextForm/' + filename)
 
-    responseDictionary = dict() # The key is the other person's message, and the value is my response
+    # The key is the other person's message, and the value is my response
+    response_dictionary = dict()
+
     # Going through each file, and recording everyone's messages to me, and my responses
-    for currentFile in allFiles:
-        myMessage, otherPersonsMessage, currentSpeaker = "","",""
-        openedFile = open(currentFile, 'r', encoding="utf8")
-        allLines = openedFile.readlines()
-        for index,lines in enumerate(allLines):
+    for current_file in all_files:
+        my_message, other_people_message, current_speaker = "", "", ""
+        opened_file = open(current_file, 'r', encoding="utf8")
+        all_lines = opened_file.readlines()
+        for index, lines in enumerate(all_lines):
             # The sender's name is separated by < and >
-            leftBracket = lines.find('<')
-            rightBracket = lines.find('>')
+            left_bracket = lines.find('<')
+            right_bracket = lines.find('>')
 
             # Find messages that I sent
-            if (lines[leftBracket+1:rightBracket] == personName):
-                if not myMessage:
+            if lines[left_bracket+1:right_bracket] == person_name:
+                if not my_message:
                     # Want to find the first message that I send (if I send multiple in a row)
-                    startMessageIndex = index - 1
-                myMessage += lines[rightBracket+1:]
+                    start_message_index = index - 1
+                my_message = my_message + " " + lines[right_bracket+1:]
 
-            elif myMessage:
+            elif my_message:
                 # Now go and see what message the other person sent by looking at previous messages
-                for counter in range(startMessageIndex, 0, -1):
-                    currentLine = allLines[counter]
+                for counter in range(start_message_index, 0, -1):
+                    current_line = all_lines[counter]
                     # In case the message above isn't in the right format
-                    if (currentLine.find('<') < 0 or currentLine.find('>') < 0):
-                        myMessage, otherPersonsMessage, currentSpeaker = "","",""
+                    if current_line.find('<') < 0 or current_line.find('>') < 0:
+                        my_message, other_people_message, current_speaker = "", "", ""
                         break
-                    if not currentSpeaker:
+                    if not current_speaker:
                         # The first speaker not named me
-                        currentSpeaker = currentLine[currentLine.find('<')+1:currentLine.find('>')]
-                    elif (currentSpeaker != currentLine[currentLine.find('<')+1:currentLine.find('>')]):
+                        current_speaker = current_line[current_line.find('<')+1:current_line.find('>')]
+                    elif current_speaker != current_line[current_line.find('<')+1:current_line.find('>')]:
                         # A different person started speaking, so now I know that the first person's message is done
-                        otherPersonsMessage = cleanMessage(otherPersonsMessage)
-                        myMessage = cleanMessage(myMessage)
-                        responseDictionary[otherPersonsMessage] = myMessage
+                        other_people_message = clean_message(other_people_message)
+                        my_message = clean_message(my_message)
+                        response_dictionary[other_people_message] = my_message
                         break
-                    otherPersonsMessage = currentLine[currentLine.find('>')+1:] + otherPersonsMessage
-                myMessage, otherPersonsMessage, currentSpeaker = "","",""
-    return responseDictionary
+                    other_people_message = current_line[current_line.find('>')+1:] + " " + other_people_message
+                my_message, other_people_message, current_speaker = "", "", ""
+    return response_dictionary
 
-def getFacebookData():
-    responseDictionary = dict()
-    fbFile = open('fbMessages.txt', 'r', encoding="utf8")
-    allLines = fbFile.readlines()
-    myMessage, otherPersonsMessage, currentSpeaker = "","",""
-    for index,lines in enumerate(allLines):
-        rightBracket = lines.find(']') + 2
-        justMessage = lines[rightBracket:]
-        colon = justMessage.find(':')
+
+def get_facebook_data():
+    response_dictionary = dict()
+    fb_file = open('fbMessages.txt', 'r', encoding="utf8")
+    all_lines = fb_file.readlines()
+    my_message, other_people_message, current_speaker = "", "", ""
+    for index, lines in enumerate(all_lines):
+        right_bracket = lines.find(']') + 2
+        just_message = lines[right_bracket:]
+        colon = just_message.find(':')
         # Find messages that I sent
-        if (justMessage[:colon] == personName):
-            if not myMessage:
+        if just_message[:colon] == person_name:
+            if not my_message:
                 # Want to find the first message that I send (if I send multiple in a row)
-                startMessageIndex = index - 1
-            myMessage += justMessage[colon+2:]
+                start_message_index = index - 1
+            my_message = my_message + " " + just_message[colon+2:]
 
-        elif myMessage:
+        elif my_message:
             # Now go and see what message the other person sent by looking at previous messages
-            for counter in range(startMessageIndex, 0, -1):
-                currentLine = allLines[counter]
-                rightBracket = currentLine.find(']') + 2
-                justMessage = currentLine[rightBracket:]
-                colon = justMessage.find(':')
-                if not currentSpeaker:
+            for counter in range(start_message_index, 0, -1):
+                current_line = all_lines[counter]
+                right_bracket = current_line.find(']') + 2
+                just_message = current_line[right_bracket:]
+                colon = just_message.find(':')
+                if not current_speaker:
                     # The first speaker not named me
-                    currentSpeaker = justMessage[:colon]
-                elif (currentSpeaker != justMessage[:colon] and otherPersonsMessage):
+                    current_speaker = just_message[:colon]
+                elif current_speaker != just_message[:colon] and other_people_message:
                     # A different person started speaking, so now I know that the first person's message is done
-                    otherPersonsMessage = cleanMessage(otherPersonsMessage)
-                    myMessage = cleanMessage(myMessage)
-                    responseDictionary[otherPersonsMessage] = myMessage
+                    other_people_message = clean_message(other_people_message)
+                    my_message = clean_message(my_message)
+                    response_dictionary[other_people_message] = my_message
                     break
-                otherPersonsMessage = justMessage[colon+2:] + otherPersonsMessage
-            myMessage, otherPersonsMessage, currentSpeaker = "","",""
-    return responseDictionary
+                other_people_message = just_message[colon+2:] + " " + other_people_message
+            my_message, other_people_message, current_speaker = "", "", ""
+    return response_dictionary
 
-def getLinkedInData():
+
+def get_linkedin_data():
     df = pd.read_csv('Inbox.csv')
-    dateTimeConverter = lambda x: datetime.strptime(x,'%B %d, %Y, %I:%M %p')
-    responseDictionary = dict()
-    peopleContacted = df['From'].unique().tolist()
-    for person in peopleContacted:
-        receivedMessages = df[df['From'] == person]
-        sentMessages = df[df['To'] == person]
-        if (len(sentMessages) == 0 or len(receivedMessages) == 0):
+    date_time_converter = lambda x: datetime.strptime(x,'%B %d, %Y, %I:%M %p')
+    response_dictionary = dict()
+    people_contacted = df['From'].unique().tolist()
+    for person in people_contacted:
+        received_messages = df[df['From'] == person]
+        sent_messages = df[df['To'] == person]
+        if len(sent_messages) == 0 or len(received_messages) == 0:
             # There was no actual conversation
             continue
-        combined = pd.concat([sentMessages, receivedMessages])
-        combined['Date'] = combined['Date'].apply(dateTimeConverter)
+        combined = pd.concat([sent_messages, received_messages])
+        combined['Date'] = combined['Date'].apply(date_time_converter)
         combined = combined.sort(['Date'])
-        otherPersonsMessage, myMessage = "",""
-        firstMessage = True
+        other_people_message, my_message = "", ""
+        first_message = True
         for index, row in combined.iterrows():
-            if (row['From'] != personName):
-                if myMessage and otherPersonsMessage:
-                    otherPersonsMessage = cleanMessage(otherPersonsMessage)
-                    myMessage = cleanMessage(myMessage)
-                    responseDictionary[otherPersonsMessage.rstrip()] = myMessage.rstrip()
-                    otherPersonsMessage, myMessage = "",""
-                otherPersonsMessage = otherPersonsMessage + row['Content'] + " "
+            if row['From'] != person_name:
+                if my_message and other_people_message:
+                    other_people_message = clean_message(other_people_message)
+                    my_message = clean_message(my_message)
+                    response_dictionary[other_people_message.rstrip()] = my_message.rstrip()
+                    other_people_message, my_message = "", ""
+                other_people_message = other_people_message + row['Content'] + " "
             else:
-                if (firstMessage):
-                    firstMessage = False
+                if first_message:
+                    first_message = False
                     # Don't include if I am the person initiating the convo
                     continue
-                myMessage = myMessage + str(row['Content']) + " "
-    return responseDictionary
+                my_message = my_message + str(row['Content']) + " "
+    return response_dictionary
 
-def cleanMessage(message):
+
+def clean_message(message):
     # Remove new lines within message
-    cleanedMessage = message.replace('\n',' ').lower()
+    cleaned_message = message.replace('\n', ' ').lower()
     # Deal with some weird tokens
-    cleanedMessage = cleanedMessage.replace("\xc2\xa0", "")
+    cleaned_message = cleaned_message.replace("\xc2\xa0", "")
     # Remove punctuation
-    cleanedMessage = re.sub('([.,!?])','', cleanedMessage)
+    cleaned_message = re.sub('([.,!?])', '', cleaned_message)
     # Remove multiple spaces in message
-    cleanedMessage = re.sub(' +',' ', cleanedMessage)
-    return cleanedMessage
+    cleaned_message = re.sub(' +', ' ', cleaned_message)
+    return cleaned_message
 
-combinedDictionary = {}
-if (googleData == 'y'):
+combined_dictionary = {}
+if google_data == 'y':
     print('Getting Google Hangout Data')
-    combinedDictionary.update(getGoogleHangoutsData())
-if (fbData == 'y'):
+    combined_dictionary.update(get_google_hangouts_data())
+if fb_data == 'y':
     print('Getting Facebook Data')
-    combinedDictionary.update(getFacebookData())
-if (linkedInData == 'y'):
+    combined_dictionary.update(get_facebook_data())
+if linkedin_data == 'y':
     print('Getting LinkedIn Data')
-    combinedDictionary.update(getLinkedInData())
-print('Total len of dictionary', len(combinedDictionary))
+    combined_dictionary.update(get_linkedin_data())
+print('Total len of dictionary', len(combined_dictionary))
 
 print('Saving conversation data dictionary')
-np.save('conversationDictionary.npy', combinedDictionary)
+np.save('conversationDictionary.npy', combined_dictionary)
 
-conversationFile = open('conversationData.txt', 'w', encoding="utf8")
-for key,value in combinedDictionary.items():
-    if (not key.strip() or not value.strip()):
+conversation_file = open('conversationData.txt', 'w', encoding="utf8")
+for key, value in combined_dictionary.items():
+    if not key.strip() or not value.strip():
         # If there are empty strings
         continue
-    conversationFile.write(key.strip() + value.strip())
+    conversation_file.write(key.strip() + " " + value.strip())
